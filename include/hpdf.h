c4b6a371f029e122d1365a48e95fc9467dba6e5d
Dmitry Solomennikov <dmitrys99@mail.ru>
2025-03-22 14:38:31 +0300

    Move out 3dmeasure to separate file
    Signed-off-by: Dmitry Solomennikov <dmitrys99@mail.ru>

--- include/hpdf.h
+++ include/hpdf.h
@@ -1639,50 +1639,6 @@ HPDF_Page_CreateXObjectAsWhiteRect  (HPDF_Doc   pdf,
                                      HPDF_Rect  rect);
 
 /*--------------------------------------------------------------------------*/
-/*----- 3D Measure ---------------------------------------------------------*/
-HPDF_EXPORT(HPDF_3DMeasure)
-HPDF_Page_Create3DC3DMeasure  (HPDF_Page      page,
-                               HPDF_Point3D   firstanchorpoint,
-                               HPDF_Point3D   textanchorpoint);
-
-HPDF_EXPORT(HPDF_3DMeasure)
-HPDF_Page_CreatePD33DMeasure  (HPDF_Page      page,
-                               HPDF_Point3D   annotationPlaneNormal,
-                               HPDF_Point3D   firstAnchorPoint,
-                               HPDF_Point3D   secondAnchorPoint,
-                               HPDF_Point3D   leaderLinesDirection,
-                               HPDF_Point3D   measurementValuePoint,
-                               HPDF_Point3D   textYDirection,
-                               HPDF_REAL      value,
-                               const char    *unitsString);
-
-HPDF_EXPORT(HPDF_STATUS)
-HPDF_3DMeasure_SetName  (HPDF_3DMeasure   measure,
-                         const char      *name);
-
-HPDF_EXPORT(HPDF_STATUS)
-HPDF_3DMeasure_SetColor  (HPDF_3DMeasure   measure,
-                          HPDF_RGBColor    color);
-
-HPDF_EXPORT(HPDF_STATUS)
-HPDF_3DMeasure_SetTextSize  (HPDF_3DMeasure   measure,
-                             HPDF_REAL        textsize);
-
-HPDF_EXPORT(HPDF_STATUS)
-HPDF_3DC3DMeasure_SetTextBoxSize  (HPDF_3DMeasure   measure,
-                                   HPDF_INT32       x,
-                                   HPDF_INT32       y);
-
-HPDF_EXPORT(HPDF_STATUS)
-HPDF_3DC3DMeasure_SetText  (HPDF_3DMeasure   measure,
-                            const char      *text,
-                            HPDF_Encoder     encoder);
-
-HPDF_EXPORT(HPDF_STATUS)
-HPDF_3DC3DMeasure_SetProjectionAnotation  (HPDF_3DMeasure    measure,
-                                           HPDF_Annotation   projectionanotation);
-
-/*--------------------------------------------------------------------------*/
 /*----- image data ---------------------------------------------------------*/
 
 /**
